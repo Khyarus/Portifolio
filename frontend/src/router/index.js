b4d13c83ec547f2f25a68855efb6e9f5493986bc
JOAO GABRIEL CANSI SILVEIRA <pugderpy@gmail.com>
2025-03-25 17:15:05 -0300

    Atualização correção pra testes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,29 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Projects from '../views/Projects.vue'
-import Login from '../views/Login.vue'
+import Home from '../views/Home.vue' // Note o H maiúsculo
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/projects', component: Projects, meta: { requiresAuth: true } },
-  { path: '/login', component: Login }
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
-})
-
-// Middleware de autenticação
-router.beforeEach((to, from, next) => {
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  const isAuthenticated = localStorage.getItem('token')
-  
-  if (requiresAuth && !isAuthenticated) {
-    next('/login')
-  } else {
-    next()
-  }
 })
 
 export default router
